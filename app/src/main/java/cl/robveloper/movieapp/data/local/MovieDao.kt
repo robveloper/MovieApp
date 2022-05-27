7993cb19687bd01b755795c95b3a330a56842801
Roberto Sobarzo <robveloper@gmail.com>
2022-05-27 15:36:06 -0400

    Add DAO

---
+++ app/src/main/java/cl/robveloper/movieapp/data/local/MovieDao.kt
@@ -0,0 +1,18 @@
+package cl.robveloper.movieapp.data.local
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import cl.robveloper.movieapp.data.model.MovieEntity
+
+@Dao
+interface MovieDao {
+
+    @Query("SELECT * FROM movieentity")
+    suspend fun getAllMovies(): List<MovieEntity>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun saveMovie(movie: MovieEntity)
+
+}
